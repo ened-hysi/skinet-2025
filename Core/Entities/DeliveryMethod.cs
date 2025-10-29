0ffe3a3857fb4ff98e78b649b07bd64ecccdaae5
ened-hysi <enedhysi@gmail.com>
2025-10-29 12:51:02 +0100

    End of section 16

---
+++ Core/Entities/DeliveryMethod.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Text;
+
+namespace Core.Entities;
+
+public class DeliveryMethod : BaseEntity
+{
+  public required string ShortName { get; set; }
+  public required string DeliveryTime { get; set; }
+  public required string Description { get; set; }
+  public decimal Price { get; set; }
+}
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'api/'
+    apiUrl: 'api/',
+    stripePublicKey: 'pk_test_51SKbmUEFlpzQNBqNXYHIsUYP17PmqkVF2EiW2rYWD94SwX4rCPmSyB1U5YijjhcXXFLyhg3nNBzTWvgRuGSzPzxU00K8ghgjLG'
 };
